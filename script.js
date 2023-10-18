22fdd62b9c6025c7259463a402e3aab8663aa7e0
Lion-jan <islomakapardayev75@gmail.com>
2023-10-18 20:57:57 +0500

    challange2 done

--- script.js
+++ script.js
@@ -4,24 +4,33 @@ let check;
 let submit = document.querySelector('button')
 let result = document.getElementById('result')
 let yourRate;
-rates.forEach((el)=>{
-    el.addEventListener('click',(event)=>{
+rates.forEach((el) => {
+    el.addEventListener('click', (event) => {
         event.target.classList.toggle('active');
         check = document.querySelectorAll('.active');
-        yourRate=event.target.textContent;
-        if (check.length>0){
+        yourRate = event.target.textContent;
+        if (check.length > 0) {
             submit.classList.add('complete')
         }
         else submit.classList.remove('complete')
-        console.log(check.length)
     })
 })
 
-submit.addEventListener('click',()=>{
-   if(check.length>0){
-    main.classList.add('submit')
-    result.textContent=`You selected ${yourRate} out of 5`
-   }
-   else alert('please Rate-Us')
+submit.addEventListener('click', () => {
+    if (check.length > 0) {
+        main.classList.add('submit')
+        result.textContent = `You selected ${yourRate} out of 5`
+    }
+    else alert('please Rate-Us')
+})
+
+
+rates.forEach((el) => {
+    el.addEventListener('click', (event) => {
+        rates.forEach((index) => {
+            index.classList.remove('active')
+        })
+        el.classList.add('active')
+    })
 })
 
